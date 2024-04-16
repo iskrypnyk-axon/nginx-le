77564162d5a027785306d285c86dfd4cab3b93e0
Ihor Skrypnyk <iskrypnyk@axon.dev>
2024-04-16 17:42:54 +0300

    feat: add aws integration

---
+++ script/aws.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# scripts is adding AWS Security Group rule for certificate add or renewal
+# EC2 IAM role should have attached policy which allows add and delete SG
+
+if [ "${AWS_INTEGRATED}" == "true" ]; then
+    if [ -z "$(which aws)" ]; then
+        apk add --no-cache aws-cli && echo "aws-cli installed"
+    fi
+    
+    if [ "$1" == "add" ] && [ ! -z "${AWS_REGION}" ] && [ ! -z "${AWS_SG}" ]; then
+        aws ec2 authorize-security-group-ingress --region ${AWS_REGION} --group-id "${AWS_SG}" --protocol tcp --port 80 --cidr "0.0.0.0/0" --output text
+    fi
+
+    if [ "$1" == "del" ] && [ ! -z "${AWS_REGION}" ] && [ ! -z "${AWS_SG}" ]; then
+        aws ec2 revoke-security-group-ingress --region ${AWS_REGION} --group-id "${AWS_SG}" --protocol tcp --port 80 --cidr "0.0.0.0/0" --output text
+    fi
+else
+    echo "aws integration disabled"
+fi
